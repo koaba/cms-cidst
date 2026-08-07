7e15d143aa112a05aadd4377f3b93340cc024f46
Jeritahiry <jeritahir@gmail.com>
2026-08-07 13:39:03 +0300

    Fix NewsTicker: declaration de classe manquante

--- app/Models/NewsTicker.php
+++ app/Models/NewsTicker.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NewsTicker extends Model
 {
+    use HasFactory;
     protected $fillable = ['content', 'link_url', 'order', 'is_active'];
-
     protected $casts = ['is_active' => 'boolean'];
 }
