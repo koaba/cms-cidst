939c4a2b64bc9b403013c217890f22bfdc27bf90
Jeritahiry <jeritahir@gmail.com>
2026-08-11 11:04:52 +0300

    Migre le composant bouton admin vers les classes daisyUI

--- resources/views/components/admin/button.blade.php
+++ resources/views/components/admin/button.blade.php
@@ -1,13 +1,12 @@
 @props(['href' => null, 'type' => 'submit'])
-
 @if($href)
     <a href="{{ $href }}"
-       {{ $attributes->merge(['class' => 'inline-block bg-cidst-red hover:bg-cidst-red/90 text-white px-4 py-2 rounded transition-colors']) }}>
+       {{ $attributes->merge(['class' => 'btn btn-primary text-white']) }}>
         {{ $slot }}
     </a>
 @else
     <button type="{{ $type }}"
-            {{ $attributes->merge(['class' => 'bg-cidst-red hover:bg-cidst-red/90 text-white px-4 py-2 rounded transition-colors']) }}>
+            {{ $attributes->merge(['class' => 'btn btn-primary text-white']) }}>
         {{ $slot }}
     </button>
 @endif
