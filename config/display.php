29dda9060c72dd0cef6dcb43930d94096a766fc2
Jeritahiry <jeritahir@gmail.com>
2026-08-25 13:11:57 +0300

    feat: ajoute config/media.php et config/display.php (centralisation constantes MAX_*)

---
+++ config/display.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    // Nombre maximum de sliders affichables sur la page d'accueil
+    'max_sliders' => 5,
+
+    // Sidebar "actualités"
+    'max_sidebar_articles' => 5,
+
+    // Bandeau défilant (news ticker)
+    'max_ticker_articles' => 5,
+    'ticker_recent_days' => 30,
+
+];
---
+++ config/media.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    // Galerie simple d'un article
+    'max_gallery_images' => 20,
+
+    // Diaporamas liés à un article
+    'max_diaporamas' => 4,
+    'max_images_per_diaporama' => 10,
+
+    // Vidéos liées à un article
+    'max_videos' => 5,
+    'max_video_upload_kb' => 15360, // 15 Mo
+
+    // Documents PDF (articles, catégories, documents PDF autonomes)
+    'max_pdfs' => 10,
+    'max_pdf_upload_kb' => 10240, // 10 Mo par PDF
+
+];
