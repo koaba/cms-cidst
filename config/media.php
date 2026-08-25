aaca16d8b8c07e857553cef13207cb7b11260ab9
Jeritahiry <jeritahir@gmail.com>
2026-08-25 13:21:56 +0300

    feat: ajoute config/media.php et config/display.php (centralisation constantes MAX_*)

--- config/media.php
+++ config/media.php
@@ -5,15 +5,15 @@ return [
     // Galerie simple d'un article
     'max_gallery_images' => 20,
 
-    // Diaporamas liés à un article
+    // Diaporamas liÃ©s Ã  un article
     'max_diaporamas' => 4,
     'max_images_per_diaporama' => 10,
 
-    // Vidéos liées à un article
+    // VidÃ©os liÃ©es Ã  un article
     'max_videos' => 5,
     'max_video_upload_kb' => 15360, // 15 Mo
 
-    // Documents PDF (articles, catégories, documents PDF autonomes)
+    // Documents PDF (articles, catÃ©gories, documents PDF autonomes)
     'max_pdfs' => 10,
     'max_pdf_upload_kb' => 10240, // 10 Mo par PDF
 
